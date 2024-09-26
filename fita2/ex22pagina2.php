ce83cb9564b1325b3bd0eb0d4def6740409fc19f
josehtat <jhurtadot33@gmail.com>
2024-09-26 20:18:13 +0200

    .

--- fita2/ex22pagina2.php
+++ fita2/ex22pagina2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ex22 p2</title>
 </head>
 <body>
     <h1>Llistat de comandes</h1>
